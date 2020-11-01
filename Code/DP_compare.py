ef5820c6c941d16edd5d22bc00d90afb0fb5b3e1
Dis-count <492193947@qq.com>
2020-11-01 18:46:45 +0800

    finish but the result is not great.

--- Code/DP_compare.py
+++ Code/DP_compare.py
@@ -41,6 +41,9 @@ def show(n, c, w, value):
 
 backpack = np.random.randint(3,7)  # 3-6 integer / number of backpacks
 item = np.random.randint(10,21)   # 10-20 /Number of total items
+space_backpack = np.random.randint(10,80,backpack)   # space for each backpack
+service = np.random.randint(1,4,item) # service time for each item
+length = np.random.randint(10,60,item)   # space for each item
 
 # 还需要写一个 pretreatment 的函数 进行预处理
 def pretreatment(length, space_backpack):
@@ -75,107 +78,14 @@ def pretreatment(length, space_backpack):
 
     return segmentation, order_list
 
-def multibag(backpack, item, c):
-
-    space_backpack = np.random.randint(10,80,backpack)  # space for each backpack
-    service = np.random.randint(1,4,item) # service time for each item
-    service_item = list(service) # list service time for each item
-    length = np.random.randint(10,60,item)   # space for each item
-    length_item = list(length) # list space for each item
-    # total_product = np.dot(length, servce)
-    # total_space = 24 * sum(space_backpack)
-    # c = 24 * ratio  # Capacity of time   # ratio 的方案不太行  至少对于room 和 group space 差距过大的情况 不适合
-    # ratio = total_product/ total_space   # The ratio between item space and backpack space
-    value_item = list(np.multiply(service,length)) # value for each item
-    print('共有', backpack, '个背包')
-    print('共有', item, '个物品')
-
-    rest_service = service_item
-    rest_value = value_item
-    rest_item = item
-
-    ordinal_item = [i for i in range(item)]
-
-    for i in range(backpack):
-
-        value = bag(rest_item, c, rest_service, rest_value)
-        print('\n最大价值为:', value[rest_item][c])
-        rest_x,cut_num = show(rest_item, c, rest_service, value)  #list
-        print('第', i+1, '个背包中所装物品为:')
-
-        for k in range(rest_item-1,-1,-1): # 注意这里要倒序  删除的时候才不会出问题
-            if not rest_x[k]:
-                sel = ordinal_item.pop(k) # 删除第k个元素
-                print('第', sel, '个,', end='')
-
-        rest_service = [rest_service[num] for num in range(rest_item) if rest_x[num]]
-
-        rest_value = [rest_value[num] for num in range(rest_item) if rest_x[num]]
-
-        rest_item = rest_item - cut_num
-
-        if rest_item == 0:
-            print('\nDecrease c')
-            return True
-
-    if rest_item > 0:
-        print('\nIncrease c')
-        return False # 原始版本 不分组时的连续背包求解
-
-# def mutemultibag(backpack, item, c):  # ignore the print
-#
-#     space_backpack = np.random.randint(10,80,backpack)  # space for each backpack
-#     service = np.random.randint(1,4,item) # service time for each item
-#     service_item = list(service) # list service time for each item
-#     length = np.random.randint(10,60,item)   # space for each item
-#     length_item = list(length) # list space for each item
-#     # total_product = np.dot(length, servce)
-#     # total_space = 24 * sum(space_backpack)
-#     # ratio = total_product/ total_space   # The ratio between item space and backpack space
-#     # c = 24 * ratio  # Capacity of time   # ratio 的方案不太行  至少对于room 和 group space 差距过大的情况 不适合
-#     value_item = list(np.multiply(service,length)) # value for each item
-#     # print('共有', backpack, '个背包')
-#     # print('共有', item, '个物品')
-#     rest_service = service_item
-#     rest_value = value_item
-#     rest_item = item
-#     ordinal_item = [i for i in range(item)]
-#
-#     for i in range(backpack):
-#         value = bag(rest_item, c, rest_service, rest_value)
-#         # print('\n最大价值为:', value[rest_item][c])
-#         rest_x,cut_num = show(rest_item, c, rest_service, value)  #list
-#         # print('第', i+1, '个背包中所装物品为:')
-#         for k in range(rest_item-1,-1,-1): # 注意这里要倒序  删除的时候才不会出问题
-#             if not rest_x[k]:
-#                 sel = ordinal_item.pop(k) # 删除第k个元素
-#                 # print('第', sel, '个,', end='')
-#
-#         rest_service = [rest_service[num] for num in range(rest_item) if rest_x[num]]
-#
-#         rest_value = [rest_value[num] for num in range(rest_item) if rest_x[num]]
-#
-#         rest_item = rest_item - cut_num
-#
-#         if rest_item == 0:
-#             # print('\nDecrease c')
-#             return True
-#
-#     if rest_item > 0:
-#         # print('\nIncrease c')
-#         return False
-
-def multibag2(backpack, item, c):
+def multibag2(backpack, item, c, space_backpack, service, length):
     # 从小到大根据c依次排, 可选集改变方式是去掉选出的物品,然后 加上 下一个范围的可选集
 # 排不下时, 就增大c.
-    space_backpack = np.random.randint(10,80,backpack)  # space for each backpack
-    service = np.random.randint(1,4,item) # service time for each item
     service_item = list(service) # list service time for each item
-    length = np.random.randint(10,60,item)   # space for each item
     length_item = list(length) # list space for each item
-
     segmentation, order_list = pretreatment(length, space_backpack)
-
+    segmentation.append([])
+    order_list.append([])
     value_item = list(np.multiply(service,length)) # value for each item
 
     # ordinal_item = [i for i in range(item)]
@@ -211,9 +121,9 @@ def multibag2(backpack, item, c):
 
 def di(a, b):  # 二分法(0,24)  用于调用 mutemultibag 返回c值
     c = round((a+b)/2)
-
-    while c < (b-0.5):
-        flag = multibag2(backpack, item, c)  # 这里还需要写一个 mute 的函数用于 隐藏输出
+    con = 0
+    while c < (b-0.5) or (con < 10):
+        flag = multibag2(backpack, item, c, space_backpack, service, length)  # 这里还需要写一个 mute 的函数用于 隐藏输出
         if flag > 0.5:  # Decrease c
             b = c
             c = round((a+b)/2)
@@ -221,13 +131,55 @@ def di(a, b):  # 二分法(0,24)  用于调用 mutemultibag 返回c值
         else:       # Increase c
             a = c
             c = round((a+b)/2)
+        con +=1
     return c
 
 c = di(0,24)
+c = 6
+multibag2(backpack, item, c, space_backpack, service, length)
 
-multibag2(backpack, item, c)
+def printmultibag2(backpack, item, c, space_backpack, service, length):
+    #  用于输出 各个背包的占比结果. 需要 backpack space 是升序的.
+    space_backpack = sorted(space_backpack)
+    service_item = list(service) # list service time for each item
+    length_item = list(length) # list space for each item
+
+    segmentation, order_list = pretreatment(length, space_backpack)
+    segmentation.append([])
+    order_list.append([])
+
+    value_item = list(np.multiply(service, length)) # value for each item
+
+    rest_service = [service_item[i] for i in order_list[0]]  # 相当于 item 容量
+    rest_value   = [value_item[i] for i in order_list[0]]    # 相当于 item 价值
+
+    rest_item = len(segmentation[0])  # 初始 item 数量
+    order_record = order_list[0]
+    capa_ratio = [0] * backpack
+
+    for i in range(backpack):
+
+        value = bag(rest_item, c, rest_service, rest_value)
+        # print('\n最大价值为:', value[rest_item][c])
+        rest_x,cut_num = show(rest_item, c, rest_service, value)  # list
+        print('\n第', i+1, '个背包中所装物品为:')
+        for k in range(rest_item-1,-1,-1): # 注意这里要倒序  删除的时候才不会出问题
+            if not rest_x[k]:
+                sel = order_record.pop(k) # 记录并删除第k个元素
+                print('第', sel+1, '个,', end='')
+
+        rest_service = [rest_service[num] for num in range(rest_item) if rest_x[num]] + [service_item[j] for j in order_list[i+1]]
+
+        rest_value = [rest_value[num] for num in range(rest_item) if rest_x[num]] + [value_item[j] for j in order_list[i+1]]
+        capa_ratio[i] = value[rest_item][c] / (24 * space_backpack[i])
+        rest_item = rest_item - cut_num + len(segmentation[i+1])
+        order_record = order_record + order_list[i+1]
+
+    return capa_ratio
+c = 6
+capa_ratio = printmultibag2(backpack, item, c, space_backpack, service, length)
+capa_ratio
 # Print c  but need to enlarge to 24, this in fact don't need
-
 
 # if __name__ == '__main__':
 
@@ -238,8 +190,7 @@ multibag2(backpack, item, c)
 def binPacking(s,p,n,q): # s is ServiceTime, p Groupspace, n is GroupNumber, q is space
 
     try:
-        pp = np.random.randint(10,60,n)
-        p = list(pp)
+        p = list(p)
 
         subscript_i = len(s)
         subscript_k = len(q)
@@ -258,8 +209,8 @@ def binPacking(s,p,n,q): # s is ServiceTime, p Groupspace, n is GroupNumber, q i
         m.addConstrs((x[i, k]* p[i]) <= q[k] for i in range(subscript_i) for k in range(subscript_k))
 
         # constraint 2
-        m.addConstrs((gp.quicksum(x[i, k]*s[i] for i in range(subscript_i)) <= gp.quicksum(24.5-0.5*x[i, k] for i in range(subscript_i))) for k in range(subscript_k))
-
+        # m.addConstrs((gp.quicksum(x[i, k]*s[i] for i in range(subscript_i)) <= gp.quicksum(24.5-0.5*x[i, k] for i in range(subscript_i))) for k in range(subscript_k))
+        m.addConstrs((gp.quicksum(x[i, k]*s[i] for i in range(subscript_i)) <= 24) for k in range(subscript_k))
         # constraint 3
         m.addConstrs((gp.quicksum((x[i, k]*s[i]*p[i])/(24*q[k]) for i in range(subscript_i)) >= t) for k in range(subscript_k))
 
@@ -284,26 +235,24 @@ def binPacking(s,p,n,q): # s is ServiceTime, p Groupspace, n is GroupNumber, q i
 
             for i in range(subscript_i):
                 if x_ik[i,k] >= 0.5:
-                    route[k].append(i+1)
+                    route[k].append(i)
 
         # for i in range(subscript_i):
         #     print('Group {0} service time is {1}'.format(i+1, s[i]))
         #     print('The number of people is {0}'.format(p[i]))
 
         for i in range(len(route)):
+            capa_ratio[i] = np.dot([s[j] for j in route[i][1:]], [p[j] for j in route[i][1:]])/ (24* q[i])
             print('The room {0} serves: {1}'.format(i+1, route[i][1:]))
 
         print(m.objVal)
 
-        return route
+        return route, capa_ratio
 
     except gp.GurobiError as e:
         print('Main-Error code ' + str(e.errno) + ": " + str(e))
 
     except AttributeError:
         print('Main-Encountered an attribute error')   # 输出最优情况
-service = np.random.randint(1,4,item)
-length = np.random.randint(10,60,item)
-space_backpack = np.random.randint(10,80,backpack)
 
-binPacking(service, length , item, space_backpack)
+binPacking(service, length, item, space_backpack)
